e89df1af233ff9ccdfb2089e5e701849f4181300
Nature <nature.swarup@gmail.com>
2021-07-20 18:08:22 -0400

    recent searches section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 import Nav from "./components/nav/nav.component";
 import Search from "./components/search/search.component";
+import searches from "./searchData";
 
 function App() {
+  const [recentSearches, setRecentSearches] = useState(searches);
   return (
     <div className="App">
       <Nav />
@@ -12,6 +15,22 @@ function App() {
         <div className="recentSearchContainer--header">
           <h3>Recent searches</h3>
           <button>Edit</button>
+        </div>
+        <div className="searchInfoContainer">
+          {recentSearches.map((search) => {
+            return (
+              <div className="searchInfo">
+                <div className="searchInfo--data">
+                  <p>{search.search}</p>
+                  <p>
+                    <span className="positions">{search.newPositions}</span> new
+                    positions
+                  </p>
+                </div>
+                <span class="material-icons">chevron_right</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/searchData.js
@@ -0,0 +1,24 @@
+const searches = [
+  {
+    search: "JavaScript Developer",
+    newPositions: 118,
+  },
+  {
+    search: "ReactJs Developer",
+    newPositions: 250,
+  },
+  {
+    search: "Web Developer",
+    newPositions: 339,
+  },
+  {
+    search: "Entry Level Developer",
+    newPositions: 54,
+  },
+  {
+    search: "JS Developer",
+    newPositions: 700,
+  },
+];
+
+export default searches;
